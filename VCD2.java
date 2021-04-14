bdb294233819542014f7e30074ce26c4e42fb965
bagas107 <82521244+bagas107@users.noreply.github.com>
2021-04-14 21:10:27 +0700

    Create VCD2.java

---
+++ VCD2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.praktikum.pkg2;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class VCD2 {
+public String judul,publisher;
+    int stok;
+    String aktor;
+    String sutradara;
+    String kategori;
+    
+    VCD2(String judul,String publisher, int stok){
+        this.judul=judul;
+        this.publisher=publisher;
+        this.stok=stok;
+    }
+    VCD2(){
+        
+    } 
+}
